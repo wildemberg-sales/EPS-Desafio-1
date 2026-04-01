28e52426aba0d364cfe271cc0f0a227aff61338e
Wildemberg Sales <junior_sales2010@hotmail.com>
2026-03-31 21:11:04 -0300

    feat: add development Docker setup and user home view definition

--- src/core/urls.py
+++ src/core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', users_views.home_users, name='home_users')
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,2 +1,9 @@
+import os
 
+from django.http import HttpResponse
 # Create your views here.
+
+def home_users():
+    return HttpResponse(
+        "Hello, world. You're at the users home page."
+    )
